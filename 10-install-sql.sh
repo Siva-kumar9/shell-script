775238ebf26e83416f9f6db3b46a5605d9ecd123
Siva-kumar9 <sivalucky1909@gmail.com>
2026-06-07 16:56:18 +0530

    add date

---
+++ 10-install-sql.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+USRID=$(id -u)
+
+if [ USRID -ne 0]
+then
+    echo "Not a Root User"
+    exit 1
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "Fail"
+        exit 1
+    else
+        echo "Success"
+    fi
+else
+    echo "Already Installed"
+fi
