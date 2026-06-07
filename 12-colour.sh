b2f7e081b366289a1ca52530a804f201bfcafc33
Siva-kumar9 <sivalucky1909@gmail.com>
2026-06-07 18:03:11 +0530

    add date

---
+++ 12-colour.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+USRID=$(id -u)
+
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 IS $R Fail"
+        exit 1
+    else
+        echo -e "$2 Is $G Success"
+    fi
+}
+
+if [ $USRID -ne 0 ]
+then
+    echo -e " $R Not a Root User"
+    exit 1
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y
+    VALIDATE $? "MYSQL"
+else
+    echo -e "$Y Already Installed"
+fi
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    VALIDATE $? "GIT"
+else
+    echo -e " $Y already installed"
+fi
