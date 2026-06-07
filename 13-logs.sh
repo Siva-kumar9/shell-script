5305ed628a02043db895dbfccd4ded3fc0b1fcf1
Siva-kumar9 <sivalucky1909@gmail.com>
2026-06-07 18:38:21 +0530

    add date

---
+++ 13-logs.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+USRID=$(id -u)
+
+if [ $USRID -ne 0 ]
+then
+    echo -e " $R Not a Root User"
+    exit 1
+fi
+
+
+LOG_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 IS $R Fail"
+        exit 1
+    else
+        echo -e "$2 Is $G Success"
+    fi
+}
+
+
+echo "The Time is : $TIMESTAMP" &>> $LOG_FILE_NAME
+
+
+dnf list installed mysql  &>>$LOG_FILE_NAME
+
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y  &>>$LOG_FILE_NAME
+    VALIDATE $? "MYSQL"
+else
+    echo -e "$Y Already Installed"
+fi
+
+dnf list installed git  &>>$LOG_FILE_NAME
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y &>>$LOG_FILE_NAME
+    VALIDATE $? "GIT"
+else
+    echo -e " $Y already installed"
+fi
