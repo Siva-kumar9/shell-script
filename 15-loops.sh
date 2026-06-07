e6772e226f4ed076cf66de0092c610a8c5ba41cd
Siva-kumar9 <sivalucky1909@gmail.com>
2026-06-07 19:15:29 +0530

    add date

---
+++ 15-loops.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+USRID=$(id -u)
+
+if [ $USRID -ne 0 ]
+then
+    echo -e " $R Not a Root User"
+    exit 1
+fi
+
+
+LOG_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 IS $R Fail $N"
+        exit 1
+    else
+        echo -e "$2 Is $G Success $N"
+    fi
+}
+
+
+echo "The Time is : $TIMESTAMP" &>> $LOG_FILE_NAME
+
+for package in $@
+do
+    dnf list installed $package &>>LOG_FILE_NAME
+    if [ $? -ne 0 ]
+    then
+        dnf install $package -y &>>LOG_FILE_NAME
+        VALIDATE $? "$package is installing"
+    else
+        echo "Already $package is Installed"
+    fi
+done
