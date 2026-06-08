bb462c39d27e7bfd24875a92ceae0f196089cddc
Siva-kumar9 <sivalucky1909@gmail.com>
2026-06-08 17:15:24 +0530

    Shell Shell

--- 16-delete-log-files.sh
+++ 16-delete-log-files.sh
@@ -39,3 +39,9 @@ echo "Time of Execution is : $TIMESTAMP "  &>>$LOG_FILE_NAME
 
 FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 echo "Files to delete : $FILES_TO_DELETE"
+
+while read -r file
+do
+    echo "Deleting Files"
+    rm -rf $file
+done <<< $FILES_TO_DELETE
