7655bea56bfad5a6280ce0ce880d0277cab47cce
Siva-kumar9 <sivalucky1909@gmail.com>
2026-06-09 13:57:56 +0530

    backup

--- 18-backup.sh
+++ 18-backup.sh
@@ -56,6 +56,9 @@ FILE=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 if [ -n "$FILE" ]
 then
     echo "Files are :: $FILE"
+    ZIP_FILE="$DESTINATION_DIR/app-logs-$TIMESTAMP.zip"
+    FILE=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS) | zip -@ "$ZIP_FILE"
 else
     echo "Files Olders than $DAYS Not Found...."
 fi
+
