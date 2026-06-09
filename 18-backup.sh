d36b1b47623406669b8e6a0eb535abe070f02fec
Siva-kumar9 <sivalucky1909@gmail.com>
2026-06-09 13:29:01 +0530

    backup

--- 18-backup.sh
+++ 18-backup.sh
@@ -5,7 +5,7 @@ DESTINATION_DIR=$2
 DAYS=${3:-14}
 
 
-LOG_FOLDER="/home/ec2-user/app-logs"
+LOG_FOLDER="/home/ec2-user/shellscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
