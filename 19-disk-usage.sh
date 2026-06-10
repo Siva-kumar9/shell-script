9c3270f4ff9fe47ccbd3b7c16893f1f863cd0552
Siva-kumar9 <sivalucky1909@gmail.com>
2026-06-10 15:10:37 +0530

    Disk

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -6,5 +6,5 @@ while read -r line
 do
     USAGE=$(echo $line | awk -F " " '{print $6F}')
     PARTITION=$(echo $line | awk -F " " '{print $NF}')
-    echo "Partiton :: $PARTITION  "Usage:: $USAGE"
+    echo "Partiton :: $PARTITION, "Usage:: $USAGE"
 done <<< $DISK_USAGE
