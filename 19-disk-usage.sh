07bde56501d63d7a7b14f31146177a0711cbd036
Siva-kumar9 <sivalucky1909@gmail.com>
2026-06-10 15:28:20 +0530

    Disk

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -8,7 +8,7 @@ while read -r line
 do
     USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk -F " " '{print $NF}')
-    echo "Partition: $PARTITION , Usage: $USAGE"
+    # echo "Partition: $PARTITION , Usage: $USAGE"
     if [ $USAGE -ge $DISK_TRSHLD ]
     then
         MSG+="High Disk Usage in Partition $PARTITION Usage is $USAGE \n"
